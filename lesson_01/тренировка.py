3f79dfff96d659aa16a39894fe15f537d53636b9
Vera <verak050897@yandex.ru>
2026-07-01 20:34:05 +0300

    Добавление файлов

---
+++ lesson_01/тренировка.py
@@ -0,0 +1,26 @@
+my_heigh = 156
+print(my_heigh)
+
+my_name = "Вера"
+my_name = "Вера Крючкова"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца?")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('S')
+print_letter('k')
+print_letter('y')
+print_letter('P')
+print_letter('r')
+print_letter('o')
